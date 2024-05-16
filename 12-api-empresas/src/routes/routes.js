0e46a84fa2d25469a2fea8283453231f6049f9e2
GabrielGertrudes <gabrielgertrudes151@gmail.com>
2024-05-16 20:05:54 -0300

    .

--- 12-api-empresas/src/routes/routes.js
+++ 12-api-empresas/src/routes/routes.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+// controllers
 const CargoController = require('../controllers/CargoController')
+
+// validators
+const { validarCargo } = require('../validators/CargoValidator')
 
 
 // Cargo
-router.post('/cargos', CargoController.criar)
-
-
+router.get('/cargos', CargoController.buscarTodos)
+router.get('/cargos/:id', CargoController.buscarPorID)
+router.post('/cargos', validarCargo, CargoController.criar)
+router.put('/cargos/:id', validarCargo, CargoController.atualizar)
+router.delete('/cargos/:id', CargoController.excluir)
 
 
 // Funcionarios
